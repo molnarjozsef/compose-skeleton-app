ae61c1df29d1dc04fe63280748b5860b41713389
József Molnár <jozsef.molnar@supercharge.io>
2021-02-03 00:14:58 +0100

    Add network service layer

---
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/service/NewsService.kt
@@ -0,0 +1,15 @@
+package com.jozsefmolnar.newskeletonapp.service
+
+import com.jozsefmolnar.newskeletonapp.BuildConfig
+import com.jozsefmolnar.newskeletonapp.model.api.NewsResponseApiModel
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsService {
+
+    @GET("top-headlines?country=nl")
+    suspend fun getLatestNews(
+        @Query("apiKey")
+        apiKey: String = BuildConfig.NEWSAPI_KEY,
+    ): NewsResponseApiModel
+}
