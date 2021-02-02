5e56710a6ea7c8406a806b406b64721a8250b4aa
József Molnár <jozsef.molnar@supercharge.io>
2021-02-02 23:54:41 +0100

    Add network DI module
    (cherry picked from commit 924991ca2c12b4a56e29259743c95b6b872c595e)

---
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.jozsefmolnar.newskeletonapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideBaseUrl() = "INSERT_BASE_URL"
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): Converter.Factory = MoshiConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        baseUrl: String,
+        converterFactory: Converter.Factory,
+    ): Retrofit = Retrofit.Builder()
+        .addConverterFactory(converterFactory)
+        .baseUrl(baseUrl)
+        .client(okHttpClient)
+        .build()
+}
