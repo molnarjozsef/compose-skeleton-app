b3d9e0496135957fe1955da44d6126526362dfcb
József Molnár <jozsef.molnar@supercharge.io>
2021-02-02 19:20:51 +0100

    Add navigation components

--- app/src/main/java/com/example/newskeletonapp/MainActivity.kt
+++ app/src/main/java/com/example/newskeletonapp/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.example.newskeletonapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.newskeletonapp.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
