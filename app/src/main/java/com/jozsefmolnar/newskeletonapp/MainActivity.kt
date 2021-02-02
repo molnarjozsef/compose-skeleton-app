7a3124e1a7e25cdfd777171fe3599fa4be522ab1
József Molnár <jozsef.molnar@supercharge.io>
2021-02-02 21:09:36 +0100

    Simplify MainActivity

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/MainActivity.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.jozsefmolnar.newskeletonapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
