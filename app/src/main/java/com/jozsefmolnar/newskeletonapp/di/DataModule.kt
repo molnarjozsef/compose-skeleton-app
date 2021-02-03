74967073097ec17c56c05e3135e23b4d2fb5587d
József Molnár <jozsef.molnar@supercharge.io>
2021-02-03 20:10:30 +0100

    Add DI for database

---
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/di/DataModule.kt
@@ -0,0 +1,29 @@
+package com.jozsefmolnar.newskeletonapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.jozsefmolnar.newskeletonapp.db.ArticleDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DataModule {
+
+    @Singleton
+    @Provides
+    fun provideArticleDatabase(@ApplicationContext context: Context): ArticleDatabase =
+        Room.databaseBuilder(context,
+            ArticleDatabase::class.java,
+            ArticleDatabase.DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideArticleDao(articleDatabase: ArticleDatabase) = articleDatabase.articleDao()
+}
