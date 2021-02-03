87c9845ef166a53fa7505cf366aefff333c1f917
József Molnár <jozsef.molnar@supercharge.io>
2021-02-03 20:10:11 +0100

    Add data model

---
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/model/data/ArticleDataModel.kt
@@ -0,0 +1,16 @@
+package com.jozsefmolnar.newskeletonapp.model.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+data class ArticleDataModel(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    val title: String,
+    val description: String?,
+    val url: String,
+    val urlToImage: String?,
+    val publishedAt: String,
+    val content: String?,
+)
