e5b1e4a591a8a382605f28c6b80ec39d30eaea07
József Molnár <jozsef.molnar@supercharge.io>
2021-02-03 20:10:19 +0100

    Add mapper for data model

---
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/mapper/DataModelMapper.kt
@@ -0,0 +1,30 @@
+package com.jozsefmolnar.newskeletonapp.mapper
+
+import com.jozsefmolnar.newskeletonapp.model.data.ArticleDataModel
+import com.jozsefmolnar.newskeletonapp.model.domain.Article
+import com.jozsefmolnar.newskeletonapp.util.ModelMapper
+import javax.inject.Inject
+
+class DataModelMapper @Inject constructor() : ModelMapper<ArticleDataModel, Article> {
+    override fun mapToDomainModel(model: ArticleDataModel) = Article(
+        title = model.title,
+        description = model.description,
+        url = model.url,
+        urlToImage = model.urlToImage,
+        publishedAt = model.publishedAt,
+        content = model.content
+    )
+
+    override fun mapFromDomainModel(domainModel: Article) = ArticleDataModel(
+        title = domainModel.title,
+        description = domainModel.description,
+        url = domainModel.url,
+        urlToImage = domainModel.urlToImage,
+        publishedAt = domainModel.publishedAt,
+        content = domainModel.content
+    )
+
+    fun mapToDomainModelList(models: List<ArticleDataModel>) = models.map { mapToDomainModel(it) }
+
+    fun mapFromDomainModelList(domainModels: List<Article>) = domainModels.map { mapFromDomainModel(it) }
+}
