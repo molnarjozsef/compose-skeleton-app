15b9bf2ef7fe08ce8162d5a1fdb68b9c9c7ffc5c
József Molnár <jozsef.molnar@supercharge.io>
2021-02-04 23:09:09 +0100

    Integrate the main RecyclerView

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/model/MainViewModel.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/model/MainViewModel.kt
@@ -4,37 +4,46 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jozsefmolnar.newskeletonapp.model.domain.Article
 import com.jozsefmolnar.newskeletonapp.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    repository: MainRepository,
+    private val repository: MainRepository,
 ) : ViewModel() {
 
-    private val _message = MutableLiveData<String>()
-    val message: LiveData<String>
-        get() = _message
+    private val _items = MutableLiveData<List<Article>>()
+    val items: LiveData<List<Article>>
+        get() = _items
 
     private val _loading = MutableLiveData(false)
     val loading: LiveData<Boolean>
         get() = _loading
 
-    init {
+    private fun initLatestNews() {
         viewModelScope.launch {
             _loading.value = true
             repository.fetchLatestNews()
             _loading.value = false
         }
+    }
+
+    fun fetchLatestNews() {
+        viewModelScope.launch {
+            repository.fetchLatestNews()
+        }
+    }
+
+    init {
+        initLatestNews()
 
         viewModelScope.launch {
             repository.getCachedNews()
-                .map { it.firstOrNull()?.title ?: "No internet connection" }
-                .collect { _message.value = it }
+                .collect { _items.value = it }
         }
     }
 }
--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/MainFragment.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/MainFragment.kt
@@ -9,14 +9,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jozsefmolnar.newskeletonapp.R
+import com.jozsefmolnar.newskeletonapp.adapter.ArticleAdapter
 import com.jozsefmolnar.newskeletonapp.databinding.MainFragmentBinding
 import com.jozsefmolnar.newskeletonapp.ui.model.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.main_fragment.*
+import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
+
+    @Inject
+    lateinit var adapter: ArticleAdapter
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -24,7 +31,7 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ) = DataBindingUtil
         .inflate<MainFragmentBinding>(
             inflater,
@@ -41,12 +48,19 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = ArticleAdapter()
     }
 
     override fun onResume() {
         super.onResume()
-        message.setOnClickListener {
-            navController.navigate(R.id.action_mainFragment_to_detailsFragment)
-        }
+
+        viewModel.items.observe(this, { articles ->
+            (recyclerView.adapter as? ArticleAdapter)?.dataSet = articles
+            recyclerView.adapter?.notifyDataSetChanged()
+            swipeContainer.isRefreshing = false
+        })
+
+        swipeContainer.setOnRefreshListener { viewModel.fetchLatestNews() }
     }
 }
