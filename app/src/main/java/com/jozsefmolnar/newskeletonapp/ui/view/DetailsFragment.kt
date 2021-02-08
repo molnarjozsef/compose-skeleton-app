b4d97502ffec0167cbec4199af0b3b0330a7454e
József Molnár <jozsef.molnar@supercharge.io>
2021-02-08 19:31:34 +0100

    Add details screen with integration

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/DetailsFragment.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/DetailsFragment.kt
@@ -2,6 +2,7 @@ package com.jozsefmolnar.newskeletonapp.ui.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -9,24 +10,33 @@ import androidx.fragment.app.viewModels
 import com.jozsefmolnar.newskeletonapp.R
 import com.jozsefmolnar.newskeletonapp.databinding.DetailsFragmentBinding
 import com.jozsefmolnar.newskeletonapp.ui.model.DetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
     private val viewModel: DetailsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = DataBindingUtil
-        .inflate<DetailsFragmentBinding>(
-            inflater,
-            R.layout.details_fragment,
-            container,
-            false
-        )
-        .apply {
-            vm = viewModel
-            lifecycleOwner = viewLifecycleOwner
+        savedInstanceState: Bundle?,
+    ): View {
+        arguments?.getInt("articleId")?.let {
+            viewModel.setArticleId(it)
         }
-        .root
+
+        return DataBindingUtil
+            .inflate<DetailsFragmentBinding>(
+                inflater,
+                R.layout.details_fragment,
+                container,
+                false
+            )
+            .apply {
+                vm = viewModel
+                lifecycleOwner = viewLifecycleOwner
+            }
+            .root
+
+    }
 }
