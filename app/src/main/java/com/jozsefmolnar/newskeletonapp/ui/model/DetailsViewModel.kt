d11f75cf8425f6b0fd952734291f43c2c3b2e60b
József Molnár <jozsef.molnar@supercharge.io>
2021-02-08 19:31:34 +0100

    Add details screen with integration
    (cherry picked from commit b4d97502ffec0167cbec4199af0b3b0330a7454e)

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/model/DetailsViewModel.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/model/DetailsViewModel.kt
@@ -1,16 +1,47 @@
 package com.jozsefmolnar.newskeletonapp.ui.model
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jozsefmolnar.newskeletonapp.model.domain.Article
+import com.jozsefmolnar.newskeletonapp.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 @HiltViewModel
-class DetailsViewModel @Inject constructor() : ViewModel() {
+class DetailsViewModel @Inject constructor(
+    private val repository: MainRepository,
+) : ViewModel() {
 
-    val message = ObservableField<String>()
+    private val _article = MutableLiveData<Article>()
+    val article: LiveData<Article>
+        get() = _article
+
+    var articleId = MutableStateFlow<Int?>(null)
+
+    fun setArticleId(id: Int) {
+        viewModelScope.launch {
+            articleId.emit(id)
+        }
+    }
 
     init {
-        message.set("Details Fragment")
+        viewModelScope.launch {
+            articleId
+                .filter { it != null }
+                .flatMapLatest {
+                    repository.getCachedArticle(it!!)
+                }
+                .filter { it != null }
+                .collect { _article.value = it }
+        }
     }
 }
