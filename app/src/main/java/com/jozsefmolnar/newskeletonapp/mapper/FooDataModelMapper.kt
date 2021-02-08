e8333ff7ab2fb55c43387b13d3d7b43b7750362b
József Molnár <>
2021-02-08 15:44:46 +0100

    Add id to domain model

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/mapper/FooDataModelMapper.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/mapper/FooDataModelMapper.kt
@@ -7,6 +7,7 @@ import javax.inject.Inject
 
 class FooDataModelMapper @Inject constructor() : ModelMapper<FooDataModel, Foo> {
     override fun mapToDomainModel(model: FooDataModel) = Foo(
+        id = model.id!!,
         name = model.name
     )
 
