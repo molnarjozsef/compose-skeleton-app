9c242688ee7c10df919639348bf7e3cd7ac329d1
József Molnár <>
2021-02-08 15:21:25 +0100

    Add list item divider to main screen

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/MainFragment.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/MainFragment.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jozsefmolnar.newskeletonapp.R
 import com.jozsefmolnar.newskeletonapp.adapter.ArticleAdapter
 import com.jozsefmolnar.newskeletonapp.databinding.MainFragmentBinding
 import com.jozsefmolnar.newskeletonapp.ui.model.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.main_fragment.*
+import kotlinx.android.synthetic.main.main_fragment.recyclerView
+import kotlinx.android.synthetic.main.main_fragment.swipeContainer
 import javax.inject.Inject
-
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -50,6 +52,12 @@ class MainFragment : Fragment() {
         navController = Navigation.findNavController(view)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = ArticleAdapter()
+
+        val dividerItemDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        ContextCompat.getDrawable(requireContext(), R.drawable.main_list_divider)?.let {
+            dividerItemDecoration.setDrawable(it)
+        }
+        recyclerView.addItemDecoration(dividerItemDecoration)
     }
 
     override fun onResume() {
