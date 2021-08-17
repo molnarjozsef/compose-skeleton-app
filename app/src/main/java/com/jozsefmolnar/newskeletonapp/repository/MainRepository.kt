1e4cf2ba0f7387bf5b9e3dfa7d113cff53359256
Jozsef Molnar <molnarjozsef.j@gmail.com>
2021-08-17 20:12:07 +0200

    Separate fetching and getting data from DB

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/repository/MainRepository.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/repository/MainRepository.kt
@@ -1,20 +1,34 @@
 package com.jozsefmolnar.newskeletonapp.repository
 
+import com.jozsefmolnar.newskeletonapp.db.FooDao
 import com.jozsefmolnar.newskeletonapp.mapper.FooApiModelMapper
+import com.jozsefmolnar.newskeletonapp.mapper.FooDataModelMapper
 import com.jozsefmolnar.newskeletonapp.model.domain.Foo
 import com.jozsefmolnar.newskeletonapp.service.FooService
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import timber.log.Timber
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
     private val fooService: FooService,
+    private val fooDao: FooDao,
     private val fooApiModelMapper: FooApiModelMapper,
+    private val fooDataModelMapper: FooDataModelMapper,
 ) {
 
-    suspend fun getLatestFoo(): Flow<List<Foo>> = flow {
-        val latestFoo = fooService.getLatestFoo()
-        val items = fooApiModelMapper.mapToDomainModelList(latestFoo.items)
-        emit(items)
+    fun getCachedFoo(): Flow<List<Foo>> = fooDao.getAll()
+        .map { fooDataModelMapper.mapToDomainModelList(it) }
+
+    suspend fun fetchLatestFoo() {
+        try {
+            val latestFoo = fooService.getLatestFoo()
+            val items = fooApiModelMapper.mapToDomainModelList(latestFoo.items)
+            val fooDataModels = fooDataModelMapper.mapFromDomainModelList(items)
+            fooDao.clearAll()
+            fooDao.insertAll(fooDataModels)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
     }
 }
--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/model/MainViewModel.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/model/MainViewModel.kt
@@ -1,16 +1,44 @@
 package com.jozsefmolnar.newskeletonapp.ui.model
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jozsefmolnar.newskeletonapp.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    repository: MainRepository,
+) : ViewModel() {
 
-    val message = ObservableField<String>()
+    private val _message = MutableLiveData<String>()
+    val message: LiveData<String>
+        get() = _message
+
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean>
+        get() = _loading
 
     init {
-        message.set("Main Fragment")
+        viewModelScope.launch {
+            _loading.value = true
+            withContext(Dispatchers.IO) {
+                repository.fetchLatestFoo()
+            }
+            _loading.value = false
+        }
+
+        viewModelScope.launch {
+            repository.getCachedFoo()
+                .map { it.firstOrNull()?.name ?: "No internet connection" }
+                .collect { _message.value = it }
+        }
     }
 }
