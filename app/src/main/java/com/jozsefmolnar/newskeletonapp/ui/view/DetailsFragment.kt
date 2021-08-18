b7338f0abe4584387f322c48cd0273e1880ab7ab
Jozsef Molnar <molnarjozsef.j@gmail.com>
2021-08-18 21:23:07 +0200

    feat: move argument saving to oncreate

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/DetailsFragment.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ui/view/DetailsFragment.kt
@@ -1,13 +1,8 @@
 package com.jozsefmolnar.newskeletonapp.ui.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.jozsefmolnar.newskeletonapp.R
-import com.jozsefmolnar.newskeletonapp.databinding.DetailsFragmentBinding
 import com.jozsefmolnar.newskeletonapp.ui.model.DetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,26 +11,10 @@ class DetailsFragment : BaseFragment<DetailsViewModel>(R.layout.details_fragment
 
     override val viewModel: DetailsViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         arguments?.getInt("articleId")?.let {
             viewModel.setArticleId(it)
         }
-
-        return DataBindingUtil
-            .inflate<DetailsFragmentBinding>(
-                inflater,
-                R.layout.details_fragment,
-                container,
-                false
-            )
-            .apply {
-                vm = viewModel
-                lifecycleOwner = viewLifecycleOwner
-            }
-            .root
-
     }
 }
