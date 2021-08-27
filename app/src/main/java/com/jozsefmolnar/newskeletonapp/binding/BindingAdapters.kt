4d493839bcacf1865d8212fafbc70d229593a041
József Molnár <molnarjozsef.j@gmail.com>
2021-08-28 01:41:19 +0200

    feat: add image loading with Glide

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/binding/BindingAdapters.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/binding/BindingAdapters.kt
@@ -1,7 +1,10 @@
 package com.jozsefmolnar.newskeletonapp.binding
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 @BindingAdapter("android:visibility")
 fun View.bindVisibility(visible: Boolean?) {
@@ -11,5 +14,15 @@ fun View.bindVisibility(visible: Boolean?) {
         } else {
             View.GONE
         }
+    }
+}
+
+@BindingAdapter("imageUrl")
+fun ImageView.bindImageUrl(imageUrl: String?) {
+    imageUrl?.let {
+        Glide.with(context)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(this)
     }
 }
