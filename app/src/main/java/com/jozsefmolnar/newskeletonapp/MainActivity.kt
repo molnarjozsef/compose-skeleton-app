aa1bf54603ab46d19c901568d73f485daa9278b3
József Molnár <molnarjozsef.j@gmail.com>
2021-08-28 02:30:36 +0200

    feat: use actionbar with navigation

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/MainActivity.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/MainActivity.kt
@@ -1,7 +1,26 @@
 package com.jozsefmolnar.newskeletonapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
+    }
+}
